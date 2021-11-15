e6c9b8b54728919291d9284c5a61f6cb920d99dc
Asher Gilani <gilaniasher@gmail.com>
2021-11-14 21:11:41 -0500

    Prepared and uploaded initial build to PyPi!
    1. Had to modify the setup.py a lot to get this to work and to render out a README
    2. I might still need to add 'wheel' as a pip dependency to resolve bdist not found error
    3. Had to add a manifest.in file to get the header files to show up properly
    4. pip install works well on linux if I have wheel, but windows complains since it needs some build tools. Might have to build a wheel separately for that and upload

--- src/setup.py
+++ src/setup.py
@@ -1,12 +1,24 @@
 '''
 This script actually compiles our C code
-Comes from here: https://docs.python.org/3/extending/building.html#building
+Build/Deploy Instructions: https://realpython.com/pypi-publish-python-package/
 
 Build:
-  python3 setup.py build
+  sudo python3 setup.py sdist
+    This just generates the .tar.gz not wheel files.
+    The wheel files make the install process quicker
 
-Installing the library:
-  sudo python3 setup.py install
+  sudo python3 setup.py sdist bdist_wheel
+    The above produces a specific linux wheel. Need to run several commands to
+    generate the manylinux wheel which PyPi allows
+
+  docker pull quay.io/pypa/manylinux_2_24_x86_64:latest
+  docker run -v .../seg-tree-c-python:/mnt/segtree -it 045b8d48f0c4
+  /opt/python/cp36-cp36m/bin/pip3 wheel src/ -w src/output
+  auditwheel repair output/c_segment_tree-1.0-cp36-cp36m-linux_x86_64.whl -w output/
+
+Verify: twine check dist/*
+Deploy TestPyPi: twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+Deploy PyPi: twine upload  dist/*
 
 Using:
   import segtree
@@ -18,7 +30,22 @@ Using:
   obj.min(0, 2)
 '''
 
-from distutils.core import setup, Extension
+from pathlib import Path
+# from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
-module1 = Extension('segtree', sources=['segtreemodule.c', 'segtree.c'])
-setup(name='segtree', version='1.0', description='This is a Segment Tree package', ext_modules=[module1])
+here = Path(__file__).parent
+README = (here / 'README.md').read_text()
+module1 = Extension('segtree', sources=['segtreemodule.c', 'segtree.c'], headers=['./segtree.h'])
+
+setup(
+  name='c-segment-tree',
+  version='1.0',
+  description='Segment Tree package written in C',
+  long_description=README,
+  long_description_content_type='text/markdown',
+  url='https://github.com/gilaniasher/segtree-c-python',
+  author='Asher Gilani',
+  author_email='gilaniasher@gmail.com',
+  ext_modules=[module1]
+)
